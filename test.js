5392c2e22916852d9172651b83562bcf1ce72372
Domryuken <ladonjackson@hotmail.co.uk>
2021-05-22 22:00:13 +0100

    testing stuff

---
+++ test.js
@@ -0,0 +1,190 @@
+
+var controller;
+
+var context = document.querySelector("canvas").getContext("2d");
+
+var screen = {
+  width: 1400,
+  height: 900
+}
+
+context.canvas.width = screen.width;
+context.canvas.height = screen.height;
+
+var player = {
+  x: 470,
+  y: 770,
+  xv: 0,
+  yv: 0,
+  height: 20,
+  width: 20,
+  x2: function(){return this.x + this.width;},
+  y2: function(){return this.y + this.height;},
+  friction: 0.9,
+  speed: 0.3
+};
+
+var box = {
+  x: 500,
+  y: 800,
+  xv: 0,
+  yv: 0,
+  height: 40,
+  width: 40,
+  x2: function(){return this.x + this.width;},
+  y2: function(){return this.y + this.height;},
+  friction: 0.9
+}
+
+controller = {
+  up: false,
+  left: false,
+  right: false,
+  down: false,
+  keyListener: function(event) {
+    var key_state = (event.type == "keydown")?true:false;
+    switch(event.keyCode) {
+      case 37:
+        controller.left = key_state;
+        break;
+      case 38:
+        controller.up = key_state;
+        break;
+      case 39:
+        controller.right = key_state;
+        break;
+      case 40:
+        controller.down = key_state;
+        break;
+    }
+  }
+};
+
+var loop = function(){
+
+  
+  handleMovement();
+  handleBoxCollision(player);
+  
+  handleScreenEdge(player);
+  handleScreenEdge(box);
+  drawScreen();
+
+  
+  player.yv *= player.friction;
+  player.xv *= player.friction;
+  
+  box.yv *= box.friction;
+  box.xv *= box.friction;
+  // printCollision();
+  // setTimeout(function(){window.requestAnimationFrame(loop);}, 1000/20)
+  window.requestAnimationFrame(loop);
+}
+
+var handleMovement = function(){
+
+  if (controller.left) {player.xv -= player.speed;}
+  if (controller.up) {player.yv -= player.speed;}
+  if (controller.right) {player.xv += player.speed;}
+  if (controller.down) {player.yv += player.speed;}
+
+  player.y += player.yv;
+  player.x += player.xv;
+}
+
+var handleBoxCollision = function(thing){
+
+  if(thing.x < box.x2() && thing.x2() > box.x && thing.y <= box.y2() && thing.y2() >= box.y){
+        
+    let diffx = thing.x - box.x2();
+    let diffx2 = thing.x2() - box.x;
+    let diffy = thing.y - box.y2();
+    let diffy2 = thing.y2() - box.y;
+    let hori = Math.min(Math.abs(diffx), Math.abs(diffx2));
+    let vert = Math.min(Math.abs(diffy), Math.abs(diffy2));
+
+    if (hori < vert) {
+      box.xv = thing.xv;
+    } else {
+      box.yv = thing.yv;
+    }
+  }
+  
+  box.y += box.yv;
+  box.x += box.xv;
+}
+
+var handleScreenEdge = function(thing){
+
+  if (thing.x <= 0) {
+    if(thing.xv > -0.5) {
+      thing.x = 0;
+      thing.xv = 0;
+    } else {
+      thing.x -= thing.xv;
+      thing.xv *= -1;
+    }
+  } else if(thing.x2() >= screen.width) {
+    if(thing.xv < 0.5) {
+      thing.x = screen.width - thing.width;
+      thing.xv = 0;
+    } else {
+      thing.x -= thing.xv;
+      thing.xv *= -1;
+    }
+  }
+  
+  if (thing.y <= 0) {
+    if(thing.yv > -0.5) {
+      thing.y = 0;
+      thing.yv = 0;
+    } else {
+      thing.y -= thing.yv;
+      thing.yv *= -1;
+    }
+  } else if(thing.y2() >= screen.height) {
+    if(thing.yv < 0.5) {
+      thing.y = screen.height - thing.height;
+      thing.yv = 0;
+    } else {
+      thing.y -= thing.yv;
+      thing.yv *= -1;
+    }
+  }
+}
+
+var drawScreen = function(){
+  
+  context.fillStyle = "black";
+  context.fillRect(0, 0, screen.width, screen.height);
+  context.fillStyle = "red";
+  context.fillRect(player.x, player.y, player.width, player.height);
+  context.fillStyle = "green";
+  context.fillRect(box.x, box.y, box.width, box.height);
+}
+
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
+
+
+
+
+var printCollision = function(){
+  var print =
+  "player x more than box x: " + (player.xv > box.xv) + 
+  "\n  player x = " + player.x + 
+  "\n player x2 = " + player.x2() +
+  "\n player xv = " + player.xv +
+  "\n  player y = " + player.y + 
+  "\n player y2 = " + player.y2() + 
+  "\n player yv = " + player.yv + 
+  "\n     box x = " + box.x + 
+  "\n    box x2 = " + box.x2() +
+  "\n    box xv = " + box.xv +
+  "\n     box y = " + box.y +
+  "\n    box y2 = " + box.y2();
+  "\n    box yv = " + box.yv;
+
+  console.log(print);
+}
