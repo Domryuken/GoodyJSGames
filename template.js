6564d431fbfc30a501952c30167717fce52778d0
Domryuken <ladonjackson@hotmail.co.uk>
2021-05-23 13:16:28 +0100

    template

---
+++ template.js
@@ -0,0 +1,52 @@
+
+var context = document.querySelector("canvas").getContext("2d");
+
+var screen = {
+  width: 1400,
+  height: 900
+}
+
+context.canvas.width = screen.width;
+context.canvas.height = screen.height;
+
+var controller = {
+  up: false,
+  left: false,
+  right: false,
+  down: false,
+  keyListener: function(event) {
+    var key_state = (event.type == "keydown")?true:false;
+    switch(event.keyCode) {
+      case 37:
+        controller.left = key_state;
+        break;
+      case 38:
+        controller.up = key_state;
+        break;
+      case 39:
+        controller.right = key_state;
+        break;
+      case 40:
+        controller.down = key_state;
+        break;
+    }
+  }
+};
+
+var loop = function(){
+  drawScreen();
+  // setTimeout(function(){window.requestAnimationFrame(loop);}, 1000/20)
+  window.requestAnimationFrame(loop);
+}
+
+
+
+var drawScreen = function(){
+  
+  context.fillStyle = "black";
+  context.fillRect(0, 0, screen.width, screen.height);
+}
+
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
